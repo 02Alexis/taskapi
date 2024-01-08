c36843c03d07466c9decc0f01d6161ac5faec998
02Alexis <m_chael1012@hotmail.com>
2024-01-08 15:20:35 -0500

    creando crud del controlador task, pero sin consultar la bd

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Get, Put, Delete } from '@nestjs/common';
+import { TaskService } from './task.service';
 
 @Controller('task')
-export class TaskController {}
+export class TaskController {
+  constructor(private tasksService: TaskService) {}
+
+  @Get()
+  findAll() {
+    return 'Get all task';
+  }
+
+  @Get(':id')
+  findOne() {
+    return 'Get one task';
+  }
+
+  @Post()
+  Create() {
+    return 'Create task';
+  }
+
+  @Delete(':id')
+  delete() {
+    return 'Delete task';
+  }
+
+  @Put(':id')
+  update() {
+    return 'Update task';
+  }
+}
