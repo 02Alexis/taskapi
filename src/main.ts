9fa3a652e22a6f12c9b7e060f46a809bab1530f1
02Alexis <m_chael1012@hotmail.com>
2024-01-08 17:25:12 -0500

    Cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); // habilitamos cors para permitir solicitudes desde otros dominios
+  app.setGlobalPrefix('api'); // Establece un prefijo global para las rutas de la API
   app.useGlobalPipes(new ValidationPipe()); // Utiliza un tubo de validación global para validar automáticamente las solicitudes
   await app.listen(3000);
 }
