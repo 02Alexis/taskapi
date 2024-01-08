53d834763494c8b503262b427b8713a94cfa27a7
02Alexis <m_chael1012@hotmail.com>
2024-01-08 15:12:56 -0500

    definiendo variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TaskModule } from './task/task.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
+    // Configura los módulos que serán utilizados en la aplicación, variables de entorno
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
     MongooseModule.forRoot(process.env.DB_URI), // Configura la conexión a la base de datos MongoDB
     TaskModule,
   ],
